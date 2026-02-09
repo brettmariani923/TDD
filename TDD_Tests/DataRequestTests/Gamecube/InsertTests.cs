541e737cb272cbf9098d79a305bdc81a305dc219
brettmariani923@gmail.com <brettmariani923@gmail.com>
2026-02-08 21:31:35 -0600

    TDD overview

--- TDD_Tests/DataRequestTests/Gamecube/InsertTests.cs
+++ TDD_Tests/DataRequestTests/Gamecube/InsertTests.cs
@@ -1,7 +1,19 @@
-﻿namespace TDD.Tests.DataRequestTests.Gamecube
+﻿using TDD.Application.Gamecube.Services;
+
+namespace TDD.Tests.DataRequestTests.Gamecube
 {
     public class InsertTests
     {
+        [Fact]
+        public void AddGame_ShouldStoreGame()
+        {
+            var service = new GamecubeService();
+            service.AddGame("Metroid Prime");
+
+            service.GetAllGames()
+                .Should()
+                .Contain(g => g.Name == "Metroid Prime");
+        }
 
     }
 }
